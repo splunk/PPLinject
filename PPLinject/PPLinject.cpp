0bc27c23831eca591b01d0eeda550373b9f189e4
Kevin O'Reilly <kevoreilly@gmail.com>
2022-07-06 17:25:27 +0100

    Initial commit

---
+++ PPLinject/PPLinject.cpp
@@ -0,0 +1,22 @@
+#include "exploit.h"
+
+#include <iostream>
+
+BOOL g_bVerbose = FALSE;
+BOOL g_bDebug = FALSE;
+BOOL g_bForce = FALSE;
+DWORD g_dwProcessId = 0;
+LPWSTR g_pwszDllPath = NULL;
+LPWSTR g_pwszProcessName = NULL;
+
+int wmain(int argc, wchar_t* argv[])
+{
+    if (!ParseArguments(argc, argv))
+        return 1;
+
+    PrintArguments();
+
+	InjectDll(g_dwProcessId, g_pwszDllPath);
+
+    return 0;
+}
